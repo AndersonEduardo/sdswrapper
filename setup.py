90cde4d9df6f3526a352c14eab8260d7b4f091fb
Anderson Aires Eduardo <anderson.eduardo@einstein.br>
2025-04-26 20:11:46 -0300

    v0.2.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sdswrapper',
-    version='0.2.3',
+    version='0.2.4',
     description='A Python package for spatial data science workflows.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -10,6 +10,7 @@ setup(
     author_email='andersonaed@example.com',
     url='https://github.com/AndersonEduardo/sdswrapper',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'numpy',
         'pandas',
